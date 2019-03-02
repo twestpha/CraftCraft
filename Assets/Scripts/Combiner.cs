b1b5aea497d8a95a098cec387aa72dc03a697395
Chris Rice <chrisrice93@gmail.com>
2019-03-01 23:59:24 -0700

    wrote combiner logic
    takes list of items on left and right along with energy and checks if
    those ingredients match a recipe. If they do, the items in the detectors
    are deleted and new items are the spawn according to the recipe.

---
+++ Assets/Scripts/Combiner.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CombinerResult {
+    public bool Success;
+    public int LeftoverEnergy;
+    public List<TileType> CreatedTiles;
+}
+
+public class Combiner {
+    // Recipe declarations (is there a way to do this from a file instead of
+    // hard-coded?)
+    public Recipe[] recipes = new Recipe[]{
+        new Recipe{
+            aTiles = new Dictionary<TileType, int>{
+                {TileType.Circle, 1},
+            },
+            bTiles = new Dictionary<TileType, int>{
+                {TileType.Square, 1},
+            },
+            produces = new List<TileType>{TileType.RoundedSquare},
+            energyRequired = 1,
+        },
+        new Recipe{
+            aTiles = new Dictionary<TileType, int>{
+                {TileType.Square, 1},
+            },
+            bTiles = new Dictionary<TileType, int>{
+                {TileType.Square, 1},
+            },
+            produces = new List<TileType>{TileType.Circle},
+            energyRequired = 1,
+        },
+        new Recipe{
+            aTiles = new Dictionary<TileType, int>{
+                {TileType.Square, 1},
+                {TileType.RoundedSquare, 1},
+            },
+            bTiles = new Dictionary<TileType, int>{
+                {TileType.Circle, 1},
+            },
+            produces = new List<TileType>{TileType.Square, TileType.Square, TileType.Square},
+            energyRequired = 2,
+        },
+    };
+
+    public CombinerResult CombineTiles(List<TileType> left, List<TileType> right, int energy){
+        // Fail early if there are no tiles on one of the sides
+        if (left.Count == 0 || right.Count == 0) {
+            return new CombinerResult{
+                Success = false,
+            };
+        }
+
+        // Check tiles against known recipes
+        foreach (var recipe in recipes) {
+            if (recipe.IngredientsMatch(left, right, energy)) {
+                return new CombinerResult{
+                    Success = true,
+                    LeftoverEnergy = energy - recipe.energyRequired,
+                    CreatedTiles = recipe.produces,
+                };
+            }
+        }
+
+        return new CombinerResult{
+            Success = false,
+        };
+    }
+}
+
+public class Recipe {
+    public int energyRequired;
+    public Dictionary<TileType, int> aTiles;
+    public Dictionary<TileType, int> bTiles;
+    public List<TileType> produces;
+
+    public bool IngredientsMatch(List<TileType> leftTiles, List<TileType> rightTiles, int energy) {
+        if (energy < energyRequired) {
+            return false;
+        }
+
+        var leftDict = buildIngredientDictFromList(leftTiles);
+        var rightDict = buildIngredientDictFromList(rightTiles);
+
+
+        // Check if the ingredients match in either order
+        bool match = (dictsAreSame(aTiles, leftDict) && dictsAreSame(bTiles, rightDict));
+        match = match || (dictsAreSame(bTiles, leftDict) && dictsAreSame(aTiles, rightDict));
+
+        return match;
+    }
+
+    private Dictionary<TileType, int> buildIngredientDictFromList(List<TileType> ingredients) {
+        var dict = new Dictionary<TileType, int>();
+        foreach(var ingredient in ingredients) {
+            if (dict.ContainsKey(ingredient)) {
+                dict[ingredient]++;
+            } else {
+                dict[ingredient] = 1;
+            }
+        }
+        return dict;
+    }
+
+    static bool dictsAreSame(Dictionary<TileType, int> a, Dictionary<TileType, int> b) {
+        if (a.Count != b.Count) {
+            return false;
+        }
+
+        foreach(var entry in a) {
+            if (!b.ContainsKey(entry.Key)) {
+                return false;
+            }
+            if ( a[entry.Key] != b[entry.Key] ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
--- Assets/Scripts/DetectorComponent.cs
+++ Assets/Scripts/DetectorComponent.cs
@@ -12,7 +12,6 @@ public class DetectorComponent : MonoBehaviour {
     }
 
     void Update(){
-
     }
 
     // on collision with tiles, add them to list
@@ -29,6 +28,13 @@ public class DetectorComponent : MonoBehaviour {
         }
     }
 
+    public void DestroyAllTiles() {
+        for (int i = 0; i < tiles.Length; i++) {
+            Destroy(tiles[i]);
+            tiles[i] = null;
+        }
+    }
+
     // on collision end with tiles, remove them from list
     void OnTriggerExit(Collider other){
         for(int i = 0; i < MAX_TILE_COUNT; ++i){
@@ -36,5 +42,31 @@ public class DetectorComponent : MonoBehaviour {
                 tiles[i] = null;
             }
         }
+    }
+
+    public List<TileType> getAllValidTiles() {
+        var tiles = new List<TileType>();
+        for (int j = 0; j < this.tiles.Length; j++) {
+            if (this.tiles[j] != null) {
+                TileComponent tc = this.tiles[j].GetComponent<TileComponent>();
+                if (tc != null && tc.type != TileType.Invalid) {
+                    tiles.Add(tc.type);
+                }
+            }
+        }
+        return tiles;
+    }
+
+    public int getNumEnergyTiles() {
+        int numEnergy = 0;
+        for (int j = 0; j < tiles.Length; j++) {
+            if (tiles[j] != null) {
+                TileComponent tc = tiles[j].GetComponent<TileComponent>();
+                if (tc != null && tc.type == TileType.Energy) {
+                    numEnergy++;
+                }
+            }
+        }
+        return numEnergy;
     }
 }
