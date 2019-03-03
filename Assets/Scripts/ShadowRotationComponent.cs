2882b3951dd771dfaa81e9961c8cf4c90a392a7e
Trevor Westphal <trevorawestphal@gmail.com>
2019-03-02 21:51:49 -0800

    slightly better fx, slightly better ground texture

---
+++ Assets/Scripts/ShadowRotationComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowRotationComponent : MonoBehaviour {
+
+    public float xrate;
+    public float yrate;
+
+    private MeshRenderer meshrenderer;
+
+    void Start(){
+        meshrenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update(){
+        meshrenderer.material.SetTextureOffset("_MainTex", new Vector2(Time.time * xrate, Time.time * yrate));
+    }
+}
